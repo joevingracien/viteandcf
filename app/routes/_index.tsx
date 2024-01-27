00e9ff34aa7778acad3ead53cc03815dd6791183
joBar <jg.getintouch@gmail.com>
2024-01-27 17:31:00 +0100

    vite and cf ftw

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,59 +1,213 @@
+import * as THREE from "three";
+import { useRef, useReducer, useMemo } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import {
-  json,
-  type LoaderFunctionArgs,
-  type ActionFunctionArgs,
-} from "@remix-run/cloudflare";
-import { Form, useLoaderData } from "@remix-run/react";
+  useGLTF,
+  MeshTransmissionMaterial,
+  Environment,
+  Lightformer,
+} from "@react-three/drei";
+import {
+  CuboidCollider,
+  BallCollider,
+  Physics,
+  RigidBody,
+} from "@react-three/rapier";
+import { EffectComposer, N8AO } from "@react-three/postprocessing";
+import { easing } from "maath";
 
-const key = "__my-key__";
+const accents = ["#4060ff", "#20ffa0", "#ff4060", "#ffcc00"];
+const shuffle = (accent = 0) => [
+  { color: "#444", roughness: 0.1 },
+  { color: "#444", roughness: 0.75 },
+  { color: "#444", roughness: 0.75 },
+  { color: "white", roughness: 0.1 },
+  { color: "white", roughness: 0.75 },
+  { color: "white", roughness: 0.1 },
+  { color: accents[accent], roughness: 0.1, accent: true },
+  { color: accents[accent], roughness: 0.75, accent: true },
+  { color: accents[accent], roughness: 0.1, accent: true },
+];
 
-export async function loader({ context }: LoaderFunctionArgs) {
-  const { MY_KV } = context.env;
-  const value = await MY_KV.get(key);
-  return json({ value });
-}
-
-export async function action({ request, context }: ActionFunctionArgs) {
-  const { MY_KV: myKv } = context.env;
-
-  if (request.method === "POST") {
-    const formData = await request.formData();
-    const value = formData.get("value") as string;
-    await myKv.put(key, value);
-    return null;
-  }
-
-  if (request.method === "DELETE") {
-    await myKv.delete(key);
-    return null;
-  }
-
-  throw new Error(`Method not supported: "${request.method}"`);
-}
-
-export default function Index() {
-  const { value } = useLoaderData<typeof loader>();
+export default function Page() {
   return (
-    <div>
-      <h1>Welcome to Remix</h1>
-      {value ? (
-        <>
-          <p>Value: {value}</p>
-          <Form method="DELETE">
-            <button>Delete</button>
-          </Form>
-        </>
-      ) : (
-        <>
-          <p>No value</p>
-          <Form method="POST">
-            <label htmlFor="value">Set value: </label>
-            <input type="text" name="value" id="value" required />
-            <br />
-            <button>Save</button>
-          </Form>
-        </>
-      )}
+    <div className="container">
+      <div className="nav">
+        <h1 className="label" />
+        <div />
+        <span className="caption" />
+        <div />
+        <div className="flex">
+          <a href="mailto:hello@joevingracien.com">
+            <button className="inline-flex h-12 animate-background-shine items-center justify-center rounded-full border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+              GET IN TOUCH
+            </button>
+          </a>
+        </div>
+      </div>
+      <Scene style={{ borderRadius: 20 }} />
     </div>
+  );
+}
+
+function Scene(props) {
+  const [accent, click] = useReducer((state) => ++state % accents.length, 0);
+  const connectors = useMemo(() => shuffle(accent), [accent]);
+  return (
+    <Canvas
+      onClick={click}
+      shadows
+      dpr={[1, 1.5]}
+      gl={{ antialias: false }}
+      camera={{ position: [0, 0, 15], fov: 17.5, near: 1, far: 20 }}
+      {...props}
+    >
+      <color attach="background" args={["#141622"]} />
+      <ambientLight intensity={0.4} />
+      <spotLight
+        position={[10, 10, 10]}
+        angle={0.15}
+        penumbra={1}
+        intensity={1}
+        castShadow
+      />
+      <Physics gravity={[0, 0, 0]}>
+        <Pointer />
+        {connectors.map((props, i) => (
+          <Connector key={i} {...props} />
+        ))}
+        <Connector position={[10, 10, 5]}>
+          <Model>
+            <MeshTransmissionMaterial
+              clearcoat={1}
+              thickness={0.1}
+              anisotropicBlur={0.1}
+              chromaticAberration={0.1}
+              samples={8}
+              resolution={512}
+            />
+          </Model>
+        </Connector>
+      </Physics>
+      <EffectComposer disableNormalPass multisampling={8}>
+        <N8AO distanceFalloff={1} aoRadius={1} intensity={4} />
+      </EffectComposer>
+      <Environment resolution={256}>
+        <group rotation={[-Math.PI / 3, 0, 1]}>
+          <Lightformer
+            form="circle"
+            intensity={4}
+            rotation-x={Math.PI / 2}
+            position={[0, 5, -9]}
+            scale={2}
+          />
+          <Lightformer
+            form="circle"
+            intensity={2}
+            rotation-y={Math.PI / 2}
+            position={[-5, 1, -1]}
+            scale={2}
+          />
+          <Lightformer
+            form="circle"
+            intensity={2}
+            rotation-y={Math.PI / 2}
+            position={[-5, -1, -1]}
+            scale={2}
+          />
+          <Lightformer
+            form="circle"
+            intensity={2}
+            rotation-y={-Math.PI / 2}
+            position={[10, 1, 0]}
+            scale={8}
+          />
+        </group>
+      </Environment>
+    </Canvas>
+  );
+}
+
+function Connector({
+  position,
+  children,
+  vec = new THREE.Vector3(),
+  scale,
+  r = THREE.MathUtils.randFloatSpread,
+  accent,
+  ...props
+}) {
+  const api = useRef();
+  const pos = useMemo(() => position || [r(10), r(10), r(10)], []);
+  useFrame((state, delta) => {
+    delta = Math.min(0.1, delta);
+    api.current?.applyImpulse(
+      vec.copy(api.current.translation()).negate().multiplyScalar(0.2)
+    );
+  });
+  return (
+    <RigidBody
+      linearDamping={4}
+      angularDamping={1}
+      friction={0.1}
+      position={pos}
+      ref={api}
+      colliders={false}
+    >
+      <CuboidCollider args={[0.38, 1.27, 0.38]} />
+      <CuboidCollider args={[1.27, 0.38, 0.38]} />
+      <CuboidCollider args={[0.38, 0.38, 1.27]} />
+      {children ? children : <Model {...props} />}
+      {accent && (
+        <pointLight intensity={4} distance={2.5} color={props.color} />
+      )}
+    </RigidBody>
+  );
+}
+
+function Pointer({ vec = new THREE.Vector3() }) {
+  const ref = useRef();
+  useFrame(({ mouse, viewport }) => {
+    ref.current?.setNextKinematicTranslation(
+      vec.set(
+        (mouse.x * viewport.width) / 2,
+        (mouse.y * viewport.height) / 2,
+        0
+      )
+    );
+  });
+  return (
+    <RigidBody
+      position={[0, 0, 0]}
+      type="kinematicPosition"
+      colliders={false}
+      ref={ref}
+    >
+      <BallCollider args={[1]} />
+    </RigidBody>
+  );
+}
+
+function Model({ children, color = "white", roughness = 0, ...props }) {
+  const ref = useRef();
+  const { nodes, materials } = useGLTF("/c-transformed.glb");
+  useFrame((state, delta) => {
+    easing.dampC(ref.current.material.color, color, 0.2, delta);
+  });
+  return (
+    <mesh
+      ref={ref}
+      castShadow
+      receiveShadow
+      scale={10}
+      geometry={nodes.connector.geometry}
+    >
+      <meshStandardMaterial
+        metalness={0.2}
+        roughness={roughness}
+        map={materials.base.map}
+      />
+      {children}
+    </mesh>
   );
 }
