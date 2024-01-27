afd893f69db3ade556ebf58feb7c57d229a18b92
joBar <jg.getintouch@gmail.com>
2024-01-27 20:54:27 +0100

    touch

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -40,7 +40,7 @@ export default function Page() {
         <div className="flex">
           <a href="mailto:hello@joevingracien.com">
             <button className="inline-flex h-12 animate-background-shine items-center justify-center rounded-full border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-              GET IN TOUCH
+              GET IN TOUCH!
             </button>
           </a>
         </div>
