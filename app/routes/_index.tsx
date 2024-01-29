a81d7e8336476ec0cfa69e27053b40e3c4544e3d
joBar <jg.getintouch@gmail.com>
2024-01-29 14:57:48 +0100

    bro

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,13 +2,16 @@ import { Canvas } from "@react-three/fiber";
 
 export default function Index() {
   return (
-    <Canvas>
-      <ambientLight intensity={0.1} />
-      <directionalLight color="red" position={[0, 0, 5]} />
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial />
-      </mesh>
-    </Canvas>
+    <>
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial />
+        </mesh>
+      </Canvas>
+      <h1>BRO</h1>
+    </>
   );
 }
