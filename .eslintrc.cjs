7d1c562bad7800c4a5eea18d68dfe1ab78b41a06
joBar <jg.getintouch@gmail.com>
2024-01-29 15:31:43 +0100

    brrooooo

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,3 @@
-/**
- * This is intended to be a basic starting point for linting in your app.
- * It relies on recommended configs out of the box for simplicity, but you can
- * and should modify this configuration to best suit your team's needs.
- */
-
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
@@ -46,6 +40,9 @@ module.exports = {
         "import/resolver": {
           typescript: {},
         },
+      },
+      rules: {
+        "react/no-unknown-property": ["off", { ignore: ["JSX"] }],
       },
     },
 
